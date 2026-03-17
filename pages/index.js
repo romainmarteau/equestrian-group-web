eed233ed362b675083ea92629f2ebba08d88db4e
julianbattaglino <julianbattaglino@gmail.com>
2026-03-17 17:36:59 -0300

    image og

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         <meta property="og:url" content="" />
         <meta property="og:title" content="East End Equestrian Group | Uniting the Equestrian Voice of the East End" />
         <meta property="og:description" content="A coalition of equestrians, property owners, professionals and enthusiasts dedicated to developing, preserving, and promoting the horse industry on the East End" />
-        <meta property="og:image" content="why-we-exist.png" />
+        <meta property="og:image" content="/why-we-exist.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:site_name" content="East End Equestrian Group" />
@@ -32,7 +32,7 @@ export default function Home() {
         <meta name="twitter:url" content="" />
         <meta name="twitter:title" content="East End Equestrian Group | Uniting the Equestrian Voice of the East End" />
         <meta name="twitter:description" content="A coalition of equestrians, property owners, professionals and enthusiasts dedicated to developing, preserving, and promoting the horse industry on the East End" />
-        <meta name="twitter:image" content="why-we-exist.png" />
+        <meta name="twitter:image" content="/why-we-exist.png" />
         
         {/* Additional SEO Meta Tags */}
         <meta name="language" content="English" />
